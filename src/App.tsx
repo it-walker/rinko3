c3ba6b09ea6ab42d14fd582a588e9516b0992e96
it-walker <24758599+it-walker@users.noreply.github.com>
2023-09-02 18:01:40 +0900

    #8 commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,35 @@
-import * as React from 'react'
-import { Button } from '@aws-amplify/ui-react'
+import './App.css'
+import { Button, ThemeProvider } from '@aws-amplify/ui-react'
+import '@aws-amplify/ui-react/styles.css'
+
+const theme = {
+  name: 'custom-button-theme',
+  tokens: {
+    components: {
+      button: {
+        // this will affect the font weight of all Buttons
+        fontWeight: { value: '{fontWeights.black.value}' },
+        // this will only style Buttons which are the "primary" variation
+        primary: {
+          backgroundColor: { value: 'rebeccapurple' },
+          _hover: {
+            backgroundColor: { value: 'hotpink' },
+          },
+        },
+      },
+    },
+  },
+}
 
 function App() {
   return (
-    <Button
-      ariaLabel="Add item to cart"
-      backgroundColor="#ffd811"
-      borderRadius="1rem"
-      color="black"
-      fontWeight="normal"
-      onClick={() => alert('Added to cart! ✅')}
-      size="small"
-      width="8rem"
-    >
-      Add to Cart
-    </Button>
+    <ThemeProvider theme={theme}>
+      <Button
+        variation="primary"
+      >
+        Add to Cart
+      </Button>
+    </ThemeProvider>
   )
 }
 
