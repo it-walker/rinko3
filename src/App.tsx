a30d027853e202c8cc5d9f3144ebc0ce823bc607
it-walker <24758599+it-walker@users.noreply.github.com>
2023-09-02 19:21:43 +0900

    #11 add

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,58 @@
+import './normalize.css'
 import './App.css'
-import { Button, ThemeProvider } from '@aws-amplify/ui-react'
-import '@aws-amplify/ui-react/styles.css'
+import { Button, Flex, Heading, Image, Text } from '@aws-amplify/ui-react'
+// import '@aws-amplify/ui-react/styles.css'
 
-const theme = {
-  name: 'custom-button-theme',
-  tokens: {
-    components: {
-      button: {
-        // this will affect the font weight of all Buttons
-        fontWeight: { value: '{fontWeights.black.value}' },
-        // this will only style Buttons which are the "primary" variation
-        primary: {
-          backgroundColor: { value: 'rebeccapurple' },
-          _hover: {
-            backgroundColor: { value: 'hotpink' },
-          },
-        },
-      },
-    },
-  },
-}
+// const theme = {
+//   name: 'custom-button-theme',
+//   tokens: {
+//     components: {
+//       button: {
+//         // this will affect the font weight of all Buttons
+//         fontWeight: { value: '{fontWeights.black.value}' },
+//         // this will only style Buttons which are the "primary" variation
+//         primary: {
+//           backgroundColor: { value: 'rebeccapurple' },
+//           _hover: {
+//             backgroundColor: { value: 'hotpink' },
+//           },
+//         },
+//       },
+//     },
+//   },
+// }
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <Button
-        variation="primary"
-      >
-        Add to Cart
-      </Button>
-    </ThemeProvider>
+    <Flex
+      display="flex"
+      direction={{ base: 'column', large: 'row' }}
+      maxWidth="32rem"
+      padding="1rem"
+      width="100%"
+    >
+      <Image
+        alt="Abstract art"
+        height="21rem"
+        src="https://images.unsplash.com/photo-1500462918059-b1a0cb512f1d?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987"
+        width="100%"
+      />
+      <Flex justifyContent="space-between" direction="column">
+        <Heading level={3}>Abstract art</Heading>
+        <Text>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Volutpat
+          sed cras ornare arcu dui. Duis aute irure dolor in reprehenderit in
+          voluptate velit esse.
+        </Text>
+        <Button
+          variation="primary"
+          onClick={() => alert('Added item to cart!')}
+        >
+          Add to Cart
+        </Button>
+      </Flex>
+    </Flex>
   )
 }
 
